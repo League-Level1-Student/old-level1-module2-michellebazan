bbca7a9c729f712ae4af72893d1945a4e82b09e1
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-10-17 16:53:18 -0700

    TeaMaker,Minion(WIP)

---
+++ src/Minion.java
@@ -0,0 +1,17 @@
+
+public class Minion {
+	
+	public Minion(String Name, int Eyes, String Color, String Master) {
+		
+		
+		
+	
+	class Name {
+	private Name getName = new Name();
+	public Minion getName() {
+
+		return getName();
+	}
+	}
+	}
+}
--- src/TeaMakerRunner.java
+++ src/TeaMakerRunner.java
@@ -3,8 +3,17 @@
 public class TeaMakerRunner {
 public static void main(String[] args) {
 	
-	TeaBag tea = new TeaBag("Tea");
+	TeaBag tea = new TeaBag("Passion Fruit");
+	Cup cup = new Cup();
+	Kettle ket = new Kettle();
 	
+	
+	ket.getWater();
+	ket.boil();
+	tea.getFlavor();
+	
+
+	cup.makeTea(tea, ket.getWater());
  }
 
 }
