006c283cf68b9847f3dcc2693857bb27a804f4ae
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-10-31 16:54:56 -0700

    PixelParty

--- src/extra/PixelRunner.java
+++ src/extra/PixelRunner.java
@@ -7,10 +7,10 @@ public class PixelRunner {
 		p.setColor(255, 0, 225);
 		p.drawRectangle(10, 20, 8, 25);
 		p.setColor(0, 0, 225);
-		p.drawCircle(10, 7, 12);
+		p.drawCircle(10, 7, 100);
 		p.getRandomColor();
 		p.drawTriangle(8, 7, 12, 9, 3, 4);
-		
+
 		///not working after this line
 		p.saveImage();
 		p.displayImage();
